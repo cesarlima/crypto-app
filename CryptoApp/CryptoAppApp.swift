aefa38c274cf57b7fa902eef53ea4b9796f8c2a7
Cesar Lima <cesarcamposdelima@gmail.com>
2024-07-20 17:21:37 -0300

    Create the Crypto App Project

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by MacPro on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
